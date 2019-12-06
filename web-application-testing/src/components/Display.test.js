d206faff024bad30b1ee9543b1ff9a0d421e08d1
lyndsiWilliams <kcatgirl@gmail.com>
2019-12-05 20:54:30 -0600

    Changed Display test title

--- web-application-testing/src/components/Display.test.js
+++ web-application-testing/src/components/Display.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Display } from './Display';
 
-test('Display test', () => {
+test('checks that "strike" and "ball" are displayed in Display', () => {
   const { getByText } = render(<Display />);
   const strikesDisplayCheck = getByText(/strikes/i);
   const ballsDisplayCheck = getByText(/balls/i);
